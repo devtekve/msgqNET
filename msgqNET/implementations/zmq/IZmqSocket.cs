7ce6dd5257ec78e46e446dd742ad48ef49fb32d3
devtekve <devtekve@gmail.com>
2024-11-22 19:03:28 +0100

    Improve port calculation with FNV-1a hash function
    Refactored the `GetPort` method to use the FNV-1a hash function for more consistent and reliable hash value generation. Updated the project file to include a new folder for organization enhancements.

--- msgqNET/implementations/zmq/IZmqSocket.cs
+++ msgqNET/implementations/zmq/IZmqSocket.cs
@@ -2,18 +2,29 @@ namespace msgqNET.implementations.zmq;
 
 public interface IZmqSocket
 {
+    private static ulong Fnv1AHash(string str)
+    {
+        const ulong fnvPrime = 0x100000001b3UL;
+        var hashValue = 0xcbf29ce484222325UL;
+    
+        foreach (var c in str)
+        {
+            hashValue ^= (byte)c;
+            hashValue *= fnvPrime;
+        }
+    
+        return hashValue;
+    }
+
     protected internal static int GetPort(string endpoint)
     {
-        // Create a hash from the endpoint string
-        var hashValue = endpoint.GetHashCode();
+        var hashValue = Fnv1AHash(endpoint);
 
         // Define the port range
         const int startPort = 8023;
         const int maxPort = 65535;
 
-        // Calculate the port using modulo operation
-        var port = startPort + Math.Abs(hashValue % (maxPort - startPort));
-
-        return port;
+        // Map the hash value to the valid port range
+        return startPort + (int)(hashValue % (maxPort - startPort));
     }
 }
